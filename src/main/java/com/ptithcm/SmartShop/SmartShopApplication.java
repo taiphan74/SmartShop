cb00b51708db87e6cafe9644e57defcf3153ed2a
TaiPhanCoder <lonelyguitarist2745@gmail.com>
2026-01-21 10:26:34 +0700

    first commit

---
+++ src/main/java/com/ptithcm/SmartShop/SmartShopApplication.java
@@ -0,0 +1,13 @@
+package com.ptithcm.SmartShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptithcm/SmartShop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptithcm.SmartShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
